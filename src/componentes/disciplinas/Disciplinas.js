5b5cf211bef6a2f493846eb302517722d7476b0a
Nathan <nathancaetano71@gmail.com>
2025-04-15 12:02:23 -0300

    icons adicionados

--- src/componentes/disciplinas/Disciplinas.js
+++ src/componentes/disciplinas/Disciplinas.js
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import '../../App.css'
 import './style.css'
+
+const ArrowIcon = () => <FontAwesomeIcon icon={faArrowRight} style={{colo: '#E0E0E0', }} />
 
 const Disciplinas = () => {
     const [disciplinas, setDisciplinas] = useState({});
@@ -17,12 +21,12 @@ return(
         {Object.entries(disciplinas).map(([ano, dados]) => (
         <section key={ano} className="curso container">
             <div className="section-title">
-            <h1>{dados.titulo}</h1>
-            <p>{dados.descricao}</p>
+                <h1>{dados.titulo}</h1>
+                <p>{dados.descricao}</p>
             </div>
             {dados.materias.map((materia, index) => (
             <article key={index}>
-                <h2>{materia.nome}</h2>
+                <h2><i><ArrowIcon/></i>{materia.nome}:</h2>
                 <p>{materia.descricao}</p>
             </article>
             ))}
